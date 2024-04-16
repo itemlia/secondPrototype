f9ffd50a2512db6404510f2063967104b9b88b6e
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-04-16 10:29:07 +0100

    HE TATSTE LIKE YOU ONLY SWEEEETERRRR

--- Assets/plantSeed.cs
+++ Assets/plantSeed.cs
@@ -8,10 +8,11 @@ using UnityEngine;
 public class plantSeed : MonoBehaviour
 {
    public GameObject seed;
-   
+   public Vector3 offset;
 
    public void OnMouseDrag()
    {
-      seed.transform.position = Input.mousePosition;
+      seed.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - offset);
    }
 }
+
