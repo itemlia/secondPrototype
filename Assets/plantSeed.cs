0566e60683ae56826a6cd1fbb20029234dfb5e33
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-04-17 12:14:28 +0100

    lalalalalala love all my dayssss

--- Assets/plantSeed.cs
+++ Assets/plantSeed.cs
@@ -10,16 +10,25 @@ public class plantSeed : MonoBehaviour
    public GameObject seed;
    public Vector3 offset;
 
+   public float distance;
+
    public void OnMouseDrag()
    {
       seed.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - offset);
    }
 
-   private void OnTriggerEnter(Collider other)
+   private void raycastSeed()
    {
-      if (other.CompareTag("ground"))
+      RaycastHit2D down = Physics2D.Raycast(seed.transform.position, Vector2.down, distance);
+      RaycastHit2D up = Physics2D.Raycast(seed.transform.position, Vector2.up, distance);
+      RaycastHit2D left = Physics2D.Raycast(seed.transform.position, Vector2.left, distance);
+      RaycastHit2D right = Physics2D.Raycast(seed.transform.position, Vector2.right, distance);
+
+      if (up.transform == null && down.transform == null && left.transform == null && right.transform == null) return;
+      if (up.transform.CompareTag("ground") || down.transform.CompareTag("ground") ||
+          left.transform.CompareTag("ground") || right.transform.CompareTag("ground"))
       {
-         seed.tag = "planted";
+            
       }
    }
 }
