377fae62fcd1ae490cb640bbdc9798668947a446
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-05-03 13:35:33 +0100

    cojimaaa STOP IT NOW

--- Assets/inputPrice.cs
+++ Assets/inputPrice.cs
@@ -3,12 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class inputPrice : MonoBehaviour
 {
     public static inputPrice price;
+
+    public sellProduce _sellProduce;
+    
     [SerializeField] private TMP_InputField inputField;
-    [SerializeField] private string playerPrice;
+    
+    [SerializeField] private string playerAnswer;
+    [SerializeField] private int money;
 
     private void Awake()
     {
@@ -21,9 +27,23 @@ public class inputPrice : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
     public void setPrice()
     {
-        playerPrice = inputField.text;
+        playerAnswer = inputField.text;
+
+        if (playerAnswer == "yes")
+        {
+            money = _sellProduce.price;
+            _sellProduce.textComp.text = "thank you";
+        }
+        else if (playerAnswer == "no")
+        {
+            _sellProduce.textComp.text = "please enter your asking price";
+        }
+        else
+        {
+            _sellProduce.textComp.text = "error, please retype answer";
+        }
+        
     }
 }
--- Assets/sellProduce.cs
+++ Assets/sellProduce.cs
@@ -8,15 +8,19 @@ using Image = UnityEngine.UI.Image;
 
 public class sellProduce : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI textComp;
+    public TextMeshProUGUI textComp;
 
     [SerializeField] private string sellingPrice;
-    
+
     [SerializeField] private GameObject backgroundImage;
-    
+    [SerializeField] private GameObject inputField;
+
+    public int price;
+
     public void OnMouseDown()
     {
         backgroundImage.SetActive(true);
+        inputField.SetActive(true);
         textComp.text = sellingPrice;
     }
 }
