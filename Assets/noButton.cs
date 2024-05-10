5b6051fd617e9d14a7fa3cc97cf9d3101835d2f5
lia harakis <lia.harakis@yahoo.com>
2024-05-10 14:50:07 +0100

    se bande no kime no skyo
    IT WIKRS!!!!!!!!!!!!!!!!

--- Assets/noButton.cs
+++ Assets/noButton.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class noButton : MonoBehaviour
 {
@@ -9,28 +12,38 @@ public class noButton : MonoBehaviour
 
     private string playerAnswer;
     
-    [SerializeField] private GameObject inputField;
+    [SerializeField] private TMP_InputField inputField;
     
     [SerializeField] private shopManager _shopManager;
+
+    public void OnMouseDown()
+    {
+        hagglePrice(_shopManager.currentSelling);
+    }
 
     private void hagglePrice(sellProduce sellProduce)
     {
         sellProduce.textComp.text = "please enter your asking price"; //allows player to pick price
+         
+        playerAnswer = inputField.text;
 
-        int numPrice;
-
-        int.TryParse(playerAnswer, out numPrice); //turns string into int
-
-        if ((numPrice + randomNum) <
-            bigRandomNum) //checks that number isnt too big by checking it against their price - a random value
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            _shopManager.money += numPrice;
-            _shopManager.moneyCounter.text = _shopManager.money.ToString();
-            sellProduce.textComp.text = "accepted";
-        }
-        else //if number suggested is too big their plant wont be bought
-        {
-            sellProduce.textComp.text = "denied, i will no longer buy";
+            int numPrice;
+
+            int.TryParse(playerAnswer, out numPrice); //turns string into int
+            
+            if ((numPrice + randomNum) <
+                bigRandomNum) //checks that number isnt too big by checking it against their price - a random value
+            {
+                _shopManager.money += numPrice;
+                _shopManager.moneyCounter.text = _shopManager.money.ToString();
+                sellProduce.textComp.text = "accepted";
+            }
+            else //if number suggested is too big their plant wont be bought
+            {
+                sellProduce.textComp.text = "denied, i will no longer buy";
+            }
         }
     }
 }
