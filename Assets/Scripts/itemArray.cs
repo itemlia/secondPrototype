1309caa7452c7accc1023336971934524e02b340
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-05-16 15:00:03 +0100

    therell be no more soorrow ill see u there tomorrow
    for loop doneee

--- Assets/Scripts/itemArray.cs
+++ Assets/Scripts/itemArray.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class itemArray : MonoBehaviour
 {
    public List<GameObject> boughtItems;
+
+   [SerializeField] private string currentSceneName;
    
    private void Awake()
    {
@@ -18,5 +21,18 @@ public class itemArray : MonoBehaviour
       {
          DontDestroyOnLoad(gameObject);
       }
+
+      currentSceneName = SceneManager.GetActiveScene().name;
+
+      if (currentSceneName == "level 1")
+      {
+         for(int i = 0; i < boughtItems.Count; i++)
+         {
+            Instantiate(boughtItems[i]);
+         }
+      }
+      
    }
+   
+   
 }
