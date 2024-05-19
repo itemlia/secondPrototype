567967436eb1d8b235d045d96233fd25ebae873e
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-05-19 17:33:14 +0100

    check it out now S T A Y C

--- Assets/Scripts/cutPlantPrefabVer.cs
+++ Assets/Scripts/cutPlantPrefabVer.cs
@@ -7,24 +7,39 @@ using UnityEngine.UI;
 
 public class cutPlantPrefabVer : MonoBehaviour
 {
-    [SerializeField] private GameObject waterBar;
-    [SerializeField] private Button click;
-    [SerializeField] private GameObject invSlot;
-    [SerializeField] private GameObject seed;
-
-    private void Awake()
-    {
-        invSlot = GameObject.Find("inventory slot");
-        waterBar = GameObject.Find("water b");
-        seed = gameObject;
-    }
+    // [SerializeField] private GameObject waterBar;
+    // [SerializeField] private Button click;
+    // [SerializeField] private GameObject invSlot;
+    // [SerializeField] private GameObject seed;
+    //
+    // private void Awake()
+    // {
+    //     invSlot = GameObject.Find("inventory slot");
+    //     waterBar = GameObject.Find("water b");
+    //     seed = gameObject;
+    // }
+    //
+    // public void OnMouseDown()
+    // {
+    //     waterBar.transform.localScale = new Vector3(0.0f, 0.33f, 1.0f);
+    //     
+    //     seed.transform.DetachChildren();
+    //     //seed.transform.position = invSlot.transform.position;
+    // }
+    
+    public TextMeshProUGUI textComp;
+    public GameObject yesNO;
+    public int price;
+    public bool clicked;
+    public GameObject seed;
+    
+    [SerializeField] private string sellingPrice;
 
     public void OnMouseDown()
     {
-        waterBar.transform.localScale = new Vector3(0.0f, 0.33f, 1.0f);
-        
-        seed.transform.DetachChildren();
-        seed.transform.position = invSlot.transform.position;
+        yesNO.SetActive(true);
+        textComp.text = sellingPrice;
+        clicked = true;
     }
 
 }
