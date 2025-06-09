380b74b5ba1d301502e15f95a6b0935673682d51
lia harakis <lia.harakis@yahoo.com>
2025-06-09 19:03:14 +0100

    updates

--- Assets/Scripts/exitGame.cs
+++ Assets/Scripts/exitGame.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class exitGame : MonoBehaviour
 {
-    public void OnMouseDown()
-    {
-        Application.Quit();
-    }
+  [SerializeField] private Button exit;
+
+  public void close()
+  {
+    Application.Quit();
+  }
+
+  private void Start()
+  {
+      var btn = exit.GetComponent<Button>();
+      btn.onClick.AddListener(close);
+      close();
+  }
 }
